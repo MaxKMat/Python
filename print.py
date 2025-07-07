2dc63319a782186055c9009d4ece7bdf4ce3ff90
MaxKMat <mkmat04@gmail.com>
2025-07-07 11:23:15 -0400

    input

--- print.py
+++ print.py
@@ -1,1 +1,10 @@
 print("Hello")
+print("How many states are there in the USA? 1/2/3")
+answer = input()
+print("1. 40")
+print("2. 30")
+print("3. 50")
+if answer =="3":
+    print("Correct answer")
+else:
+    print("incorrect answer")   
